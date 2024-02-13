910318ac8b1c366a9a510b3f7390708bd067654a
lyu-chunkwo <lyu.chun.kwo@gmail.com>
2024-02-13 11:49:21 +0600

    slider is complete

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function () {
-  
+  $('.slider__items').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    infinite: true,
+    dots: true,
+
+    prevArrow: '<button class="slick-btn slick-prev"><svg class="slick-svg" width="22" height=""><use xlink: href="images/result/sprite/sprite.svg#left"></use></svg></button>',
+    nextArrow: '<button class="slick-btn slick-next"><svg class="slick-svg" width="22" height=""><use xlink: href="images/result/sprite/sprite.svg#right"></use></svg></button>'
+  });
 });
